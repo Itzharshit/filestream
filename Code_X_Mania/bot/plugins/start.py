a025fc2f940a36ecfe30e4500ded3de12a962d00
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-10-15 13:12:40 +0530

    Update start.py

--- Code_X_Mania/bot/plugins/start.py
+++ Code_X_Mania/bot/plugins/start.py
@@ -50,7 +50,7 @@ async def start(b, m):
             except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="<i>Oops! Something Went Wrong.</i> <b> <a href='http://t.me/AJPyroVerseGroup'>Get Support</a></b>",
+                    text="Oops! Something Went Wrong. <b> <a href='http://t.me/AJPyroVerseGroup'>Get Support</a></b>",
                     parse_mode="HTML",
                     disable_web_page_preview=True)
                 return
